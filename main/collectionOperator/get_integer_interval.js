87832615f82e5c26d5203e39a8f06b9112e9bf40
KylinDC <inlovingdays@gmail.com>
2018-12-12 17:59:41 +0800

    finish get integer interval

--- main/collectionOperator/get_integer_interval.js
+++ main/collectionOperator/get_integer_interval.js
@@ -1,8 +1,19 @@
 'use strict';
 
 function get_integer_interval(number_a, number_b) {
-  //在这里写入代码
+  let array = [];
+  if (number_a < number_b) {
+    for (let i = number_a; i <= number_b; i++) {
+      array.push(i);
+    }
+  } else if (number_a > number_b) {
+    for (let i = number_a; i >= number_b; i--) {
+      array.push(i);
+    }
+  } else {
+    array.push(number_a);
+  }
+  return array;
 }
 
 module.exports = get_integer_interval;
-
