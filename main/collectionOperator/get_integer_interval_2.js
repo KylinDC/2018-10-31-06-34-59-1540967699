c61b7fab695d86790dfb24fe1bc034f1904f1c64
KylinDC <inlovingdays@gmail.com>
2018-12-12 21:27:14 +0800

    finish get even integer interval

--- main/collectionOperator/get_integer_interval_2.js
+++ main/collectionOperator/get_integer_interval_2.js
@@ -1,7 +1,20 @@
 'use strict';
+let collectAllEven = require('./collect_all_even');
 
 function get_integer_interval_2(number_a, number_b) {
-  //在这里写入代码
+  let array = [];
+  if (number_a < number_b) {
+    for (let i = number_a; i <= number_b; i++) {
+      array.push(i);
+    }
+  } else if (number_a > number_b) {
+    for (let i = number_a; i >= number_b; i--) {
+      array.push(i);
+    }
+  } else {
+    array.push(number_a);
+  }
+  return collectAllEven(array);
 }
 
 module.exports = get_integer_interval_2;
