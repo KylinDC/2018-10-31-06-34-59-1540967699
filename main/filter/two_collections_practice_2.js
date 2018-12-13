84deedca2e81cb50ec96604cc70d1cc851e5a504
KylinDC <inlovingdays@gmail.com>
2018-12-13 18:36:13 +0800

    finish choose no common elements

--- main/filter/two_collections_practice_2.js
+++ main/filter/two_collections_practice_2.js
@@ -1,8 +1,8 @@
 'use strict';
 
 function choose_no_common_elements(collection_a, collection_b) {
-
-  //在这里写入代码
+  let concatCollection = collection_a.concat(collection_b);
+  return concatCollection.filter(element => !(collection_a.includes(element) && collection_b.includes(element)));
 }
 
 module.exports = choose_no_common_elements;
