38b0ac40898b9ef8c86937e56561108601f1f2af
KylinDC <inlovingdays@gmail.com>
2018-12-13 20:45:01 +0800

    finish rank in ascend

--- main/map/rank_asc.js
+++ main/map/rank_asc.js
@@ -1,6 +1,10 @@
 'use strict';
-var rank_asc = function(collection){
-  return [6,5,4,3,2];
+var rank_asc = function (collection) {
+  return collection.sort(ascRank);
 };
+
+function ascRank(a, b) {
+  return a > b ? -1 : 1;
+}
 
 module.exports = rank_asc;
