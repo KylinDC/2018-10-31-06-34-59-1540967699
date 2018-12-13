b1ac52aa1a013b0f26bc59f910261a4892d82f18
KylinDC <inlovingdays@gmail.com>
2018-12-13 20:19:09 +0800

    finish number map to word

--- main/map/number_map_to_word.js
+++ main/map/number_map_to_word.js
@@ -1,6 +1,7 @@
 'use strict';
-var number_map_to_word = function(collection){
-  return ['a','b','c','d','e'];
+var number_map_to_word = function (collection) {
+  let dict = '0abcdefghijklmnopqrstuvwxyz';
+  return collection.map(x => dict[x]);
 };
 
 module.exports = number_map_to_word;
