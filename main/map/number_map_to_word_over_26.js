7c0a1760f4de9cc5235ca681a8c97a6285158d57
KylinDC <inlovingdays@gmail.com>
2018-12-13 20:35:24 +0800

    finish number map to word over 26

--- main/map/number_map_to_word_over_26.js
+++ main/map/number_map_to_word_over_26.js
@@ -1,6 +1,8 @@
 'use strict';
-var number_map_to_word_over_26 = function(collection){
-  return ['a','m','aa','ad','y','aa'];
+let decToLetter = require('../collectionOperator/dec_to_letter');
+
+var number_map_to_word_over_26 = function (collection) {
+  return collection.map(element => decToLetter(element));
 };
 
 module.exports = number_map_to_word_over_26;
