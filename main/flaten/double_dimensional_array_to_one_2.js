7f4cf6caeeb3c5685b9891dc41ac9ef02b98ecac
KylinDC <inlovingdays@gmail.com>
2018-12-13 20:11:02 +0800

    finish unique flatten array

--- main/flaten/double_dimensional_array_to_one_2.js
+++ main/flaten/double_dimensional_array_to_one_2.js
@@ -1,8 +1,10 @@
 'use strict';
+let flattenArray = require('./double_dimensional_array_to_one_1');
+let chooseNoRepeat = require('../filter/choose_no_repeat_number');
 
 function double_to_one(collection) {
-
-  //在这里写入代码
+  let flatCollection = flattenArray(collection);
+  return chooseNoRepeat(flatCollection);
 }
 
 module.exports = double_to_one;
