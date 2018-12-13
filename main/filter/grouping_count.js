8ab72aff8b9d4aac18b25a57bac4d94c7f6dd0d8
KylinDC <inlovingdays@gmail.com>
2018-12-13 19:04:52 +0800

    finish grouping count

--- main/filter/grouping_count.js
+++ main/filter/grouping_count.js
@@ -1,8 +1,15 @@
 'use strict';
 
 function grouping_count(collection) {
-
-  //在这里写入代码
+  let collectionObject = {};
+  for (let element of collection) {
+    if (collectionObject[element]) {
+      collectionObject[element] += 1;
+    } else {
+      collectionObject[element] = 1;
+    }
+  }
+  return collectionObject;
 }
 
 module.exports = grouping_count;
