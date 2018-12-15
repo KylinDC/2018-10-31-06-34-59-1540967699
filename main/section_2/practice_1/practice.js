e50470bec3ee963b3b189f933cd1ead3382c6de7
KylinDC <inlovingdays@gmail.com>
2018-12-16 00:21:00 +0800

    finish homework of section 2

--- main/section_2/practice_1/practice.js
+++ main/section_2/practice_1/practice.js
@@ -1,5 +1,19 @@
+let getGroupCount = require('../../filter/grouping_count');
+
 function count_same_elements(collection) {
-  //在这里写入代码
+  let groupCount = getGroupCount(collection);
+  let keyCountArray = [];
+  for (let key in groupCount) {
+    keyCountArray = keyCountArray.concat(new keyCount(key, groupCount[key]));
+  }
+  return keyCountArray;
+}
+
+function keyCount(key, count) {
+  let obj = {};
+  obj['key'] = key;
+  obj['count'] = count;
+  return obj;
 }
 
 module.exports = count_same_elements;
